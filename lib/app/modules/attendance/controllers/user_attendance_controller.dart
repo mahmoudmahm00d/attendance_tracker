5b0025ddd3e1dba8a2c01a2efc2ad2fe47a12d83
Mahmoud Mahmoud <MAHMOUDMAHM00D@protonmail.com>
2025-04-24 00:47:44 +0300

    feat(attendance): implement add attendance functionality
    - add method to add attendance for a user on a specific date
    - show success or error snackbar based on the result
    - retrieve attendance list after adding new attendance

--- lib/app/modules/attendance/controllers/user_attendance_controller.dart
+++ lib/app/modules/attendance/controllers/user_attendance_controller.dart
@@ -1,9 +1,12 @@
+import 'package:attendance_tracker/app/components/custom_snackbar.dart';
+import 'package:attendance_tracker/config/translations/strings_enum.dart';
 import 'package:get/get.dart';
 import 'package:attendance_tracker/app/data/models/attendance.dart';
 import 'package:attendance_tracker/app/data/models/user.dart';
 import 'package:attendance_tracker/app/data/repositories/attendance_repository.dart';
 import 'package:attendance_tracker/app/data/models/subject.dart';
 import 'package:attendance_tracker/app/services/database_execution_status.dart';
+import 'package:intl/intl.dart';
 
 class UserAttendanceController extends GetxController {
   Subject subject = Get.arguments["subject"];
@@ -20,6 +23,29 @@ class UserAttendanceController extends GetxController {
     attendance = await repository.getUserAttendance(user.id, subject.id);
     status = DatabaseExecutionStatus.success;
     update();
+  }
+
+  Future<int> add(DateTime date) async {
+    var at = DateFormat("yyyy-MM-dd").format(date);
+    var result = await repository.addAttendance(
+      Attendance(
+        id: "${subject.id}:${user.id}:$at",
+        userId: user.id,
+        subjectId: subject.id,
+        at: date,
+      ),
+    );
+
+    if (result != 0) {
+      getAttendance();
+    } else {
+      CustomSnackBar.showCustomErrorSnackBar(
+        title: Strings.error.tr,
+        message: Strings.noStudentFound.tr,
+      );
+    }
+
+    return result;
   }
 
   delete(int index) async {
