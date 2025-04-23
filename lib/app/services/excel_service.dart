e41f7651b2a045346d364ea502ec5e596f221f50
Mahmoud Mahmoud <MAHMOUDMAHM00D@protonmail.com>
2025-04-24 00:51:08 +0300

    feat(excel): implement detailed attendance report generation
    - add function to generate excel report with student attendance details
    - include subject name, attendance count, and student count in the report
    - list lecture dates and attendance status for each student

--- lib/app/services/excel_service.dart
+++ lib/app/services/excel_service.dart
@@ -158,3 +158,107 @@ List<int>? generateAttendanceReport(
   excel.setDefaultSheet("Students");
   return excel.save();
 }
+
+List<int>? generateDetailedAttendanceReport(
+  String subjectName,
+  int maxAttendanceCount,
+  List<(String, int)> dates,
+  List<DetailedUserAttendance> attendance,
+) {
+  var excel = Excel.createExcel();
+  Sheet sheet = excel['Students'];
+  var defaultSheet = excel.getDefaultSheet();
+  if (defaultSheet != null) {
+    excel.delete(defaultSheet);
+  }
+
+  sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 0)).value =
+      TextCellValue("Subject");
+  sheet.cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: 0)).value =
+      TextCellValue("Attendance Count");
+  sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 0)).value =
+      TextCellValue("Students Count");
+
+  sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 1)).value =
+      TextCellValue(subjectName);
+  sheet.cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: 1)).value =
+      TextCellValue(maxAttendanceCount.toString());
+  sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 1)).value =
+      TextCellValue(attendance.length.toString());
+
+  sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 2)).value =
+      TextCellValue("Lecture");
+  sheet.cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: 2)).value =
+      TextCellValue("At");
+  sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 2)).value =
+      TextCellValue("Attendance");
+
+  for (var i = 0; i < dates.length; i++) {
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: i + 3))
+        .value = TextCellValue((i + 1).toString().padLeft(3, "0"));
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: i + 3))
+        .value = TextCellValue(dates[i].$1);
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: i + 3))
+        .value = TextCellValue(dates[i].$2.toString());
+  }
+
+  sheet
+      .cell(CellIndex.indexByColumnRow(
+          columnIndex: 0, rowIndex: dates.length + 3))
+      .value = TextCellValue("Student");
+  sheet
+      .cell(CellIndex.indexByColumnRow(
+          columnIndex: 1, rowIndex: dates.length + 3))
+      .value = TextCellValue("Father");
+  sheet
+      .cell(CellIndex.indexByColumnRow(
+          columnIndex: 2, rowIndex: dates.length + 3))
+      .value = TextCellValue("Attendance");
+
+  for (var i = 0; i < dates.length; i++) {
+    sheet
+        .cell(CellIndex.indexByColumnRow(
+            columnIndex: 3 + i, rowIndex: dates.length + 3))
+        .value = TextCellValue((i + 1).toString().padLeft(3, "0"));
+  }
+
+  for (var i = 0; i < attendance.length; i++) {
+    sheet
+        .cell(CellIndex.indexByColumnRow(
+            columnIndex: 0, rowIndex: i + dates.length + 4))
+        .value = TextCellValue(attendance[i].name);
+    sheet
+        .cell(CellIndex.indexByColumnRow(
+            columnIndex: 1, rowIndex: i + dates.length + 4))
+        .value = TextCellValue(attendance[i].fatherName ?? "");
+    sheet
+        .cell(CellIndex.indexByColumnRow(
+            columnIndex: 2, rowIndex: i + dates.length + 4))
+        .value = TextCellValue(attendance[i].attendance.keys.length.toString());
+    for (var j = 0; j < dates.length; j++) {
+      var containsKey = attendance[i].attendance.containsKey(dates[j].$1);
+      sheet
+          .cell(CellIndex.indexByColumnRow(
+              columnIndex: 3 + j, rowIndex: i + dates.length + 4))
+          .value = TextCellValue(
+        containsKey ? "✅" : "❌",
+      );
+      sheet
+          .cell(CellIndex.indexByColumnRow(
+              columnIndex: 2 + j, rowIndex: i + dates.length + 4))
+          .cellStyle!
+          .horizontalAlignment = HorizontalAlign.Center;
+
+      //     .backgroundColor = ExcelColor.fromHexString(containsKey ? "#11dd33": "#dd3311");
+    }
+  }
+
+  sheet.setColumnAutoFit(0);
+  sheet.setColumnAutoFit(1);
+  sheet.setColumnAutoFit(2);
+  excel.setDefaultSheet("Students");
+  return excel.save();
+}
